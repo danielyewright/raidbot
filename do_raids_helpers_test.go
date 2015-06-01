74d0be3a4508acc7931ed4a426c6fa6cb9a6ff19
apokalyptik <apokalyptik@apokalyptik.com>
2015-06-01 01:42:39 +0000

    empty test stubbs

---
+++ do_raids_helpers_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func TestRaidHost(t *testing.T) {
+}
+
+func TestRaidPing(t *testing.T) {
+}
+
+func TestRaidFinish(t *testing.T) {
+}
+
+func TestRaidLeave(t *testing.T) {
+}
+
+func TestRaidAltLeave(t *testing.T) {
+}
+
+func TestRaidJoin(t *testing.T) {
+}
+
+func TestRaidAltJoin(t *testing.T) {
+}
